901c4a367a02e06b4d18a92da79230061e61c34a
Mehmet <mehmetdurdu25@hotmail.com>
2021-02-10 20:07:03 +0300

    Add project files.

---
+++ Database1/Brands.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Brands]
+(
+	[BrandId] INT NOT NULL PRIMARY KEY, 
+    [BrandName] VARCHAR(50) NULL
+)
---
+++ Database1/Colors.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Colors]
+(
+	[ColorId] INT NOT NULL PRIMARY KEY, 
+    [ColorName] VARCHAR(50) NULL
+)
